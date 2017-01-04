1e00405e4e186ec92965b4c12d89049743fa8eec
Mark Jun A. Gersaniva <nujkram@users.noreply.github.com>
2017-01-04 13:33:24 +0700

    Create sqlDatabase-Records.sql

---
+++ sqlDatabase-Records.sql
@@ -0,0 +1,124 @@
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000001',
+'SQL Tutorials',
+'John McDavis',
+'Sept. 29 (Bloomberg) -- NATO Secretary General Anders Fogh Rasmussen said nations waging the war in Afghanistan must change their tactics and promote successes or risk losing public support there and at home. 
+“Reaching our goal in Afghanistan is not guaranteed,” Rasmussen told an audience at the Atlantic Council policy group in Washington yesterday. More troops will be needed at least to train the Afghan National Security Forces, Rasmussen said, while cautioning that a revised strategy must be agreed upon before decisions are made about the additional resources.',
+'Databases',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000002',
+'PHP Dummy',
+'Mark Smith',
+'The North Atlantic Treaty Organization leads the 41-nation military campaign in Afghanistan, where the Taliban regime shielded al-Qaeda before being ousted by the U.S. after the Sept. 11, 2001, terrorist attacks. The U.S. has 65,000 troops in the country, with the remainder of the 103,000-strong foreign force contributed by NATO members and other allies.',
+'Databases',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000003',
+'VB .NET Beginners',
+'Cassius Juicy',
+'The top commander in the war, U.S. Army General Stanley McChrystal, recently submitted an assessment of the security situation in the country that recommends even more emphasis on protecting the population to make room for the countryâ€™s development. He concluded he would need more forces to carry out the strategy than the 68,000 the U.S. expects to have in Afghanistan by the end of the year. 
+
+Leaders of the nations fighting in Afghanistan must speak out more about the successes achieved in the eight years of the war to win back public support, Rasmussen said.',
+'Automotive',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000004',
+'SQL Today',
+'Karen Carrey',
+'He cited 7 million Afghan students in school, one-third of them girls, and that millions of citizens were able to vote in the nationâ€™s presidential election last month in the face of threats from the Taliban. 
+
+Fraud Allegations ',
+'Programming',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000005',
+'Programming Technique',
+'John McDavis',
+'He cited 7 million Afghan students in school, one-third of them girls, and that millions of citizens were able to vote in the nationâ€™s presidential election last month in the face of threats from the Taliban. 
+
+Fraud Allegations 
+
+The election results remain in dispute because of fraud allegations. Rasmussen said he agreed that the NATO-led alliance needs a “credible and legitimate government in Afghanistan.” 
+
+Some administration officials and members of Congress, who question whether the U.S. should step up its involvement as McChrystal recommends, cite the risk that the fraud allegations could erode the strength of the Afghan government.',
+'Programming',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000006',
+'HTML',
+'David Angel',
+'“Talking down the European and Canadian contributions as some here in the United States have done can become a self- fulfilling prophecy,” Rasmussen said. 
+
+To contact the reporter on this story: Viola Gienger in Washington at vgienger@bloomberg.net .',
+'Programming',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000007',
+'English Now and Today',
+'Lucy Lee',
+'Yes actually I want the output in similar fashion as u understood. Besides these two tables o/p will have data from some labels, textboxes & combo boxes before as well as after the tables, i.e. some data before tables then tables then some data after tables.
+Actually I have some data file with different ID for each record, for each ID have some data in two tables.
+OK I am attaching a .zip file of .mdb file having three tabels. u can understand from this .mdb file.
+If u help me for the problem as u understood, I think I could proceed further.
+Thanks. ',
+'Communication',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000008',
+'Communication Arts',
+'Lucy Lee',
+'No actually I want the output in similar fashion as u understood. Besides these two tables o/p will have data from some labels, textboxes & combo boxe',
+'Communication',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'000000009',
+'PHP and MySQL',
+'Mark Smith',
+'Your healthcare providers will want to get an accurate picture of your blood pressure and chart what happens over time. Your blood pressure rises with each heartbeat and falls when your heart relaxes between beats. While BP can change from minute to minute with changes in posture, exercise, stress or sleep, it should normally be less than 120/80 mm Hg (less than 120 systolic AND less than 80 diastolic) for an adult age 20 or over.',
+'Databases',
+5
+);
+
+INSERT INTO dblibrary.tblbooks(ISBN, Title, Author, Abstract, Category, Quantity) 
+VALUES(
+'0000000010',
+'Database Management System',
+'Douglas Smith',
+'A single high reading does not necessarily mean that you have high blood pressure. However, if readings stay at 140/90 mm Hg or above (systolic 140 or above OR diastolic 90 or above) over time, your doctor will likely want you to begin a treatment program. Such a program almost always includes lifestyle changes and often prescription medication for those with readings of 140/90 or higher.',
+'Databases',
+5
+);
+
+
+INSERT INTO dblibrary.users(`UserID`, `FirstName`, `LastName`, `MiddleName`,
+ `Address`, `ContactNo`, `EmailAdd`, `username`, `password`, `role`)
+VALUES('0001', 'Juan', 'Dela Cruz', 'Clara', 'Philippines', '0000-000',
+'juan@gmail.com', 'admin', 'admin', 'Administrator');
+
+
